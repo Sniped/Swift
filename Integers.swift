446adbb745cd7456a5f2ea7c4acf62fe7e9b9abb
Facto <33882218+Sniped@users.noreply.github.com>
2018-03-21 21:10:50 -0400

    quick mafs

---
+++ Integers.swift
@@ -0,0 +1,23 @@
+import Darwin
+
+let pi = 3.14159
+
+var myAge: Int = 13
+print("Min Int \(Int64.min)")
+print("Max Int \(Int64.max)")
+
+var pi2: Float = 3.1415
+var pi3: Double = 3.1415
+
+print("Min Double \(DBL_MIN)")
+print("Max Double \(DBL_MAX)")
+
+print("Max Float \(Float.greatestFiniteMagnitude)")
+var canVote: Bool = true
+var myGrade: Character = "A"
+
+var three: Double = 3.0
+var two: Int = 2
+
+var five = three + Double(two)
+print("3 : \(Int(3.14))")
